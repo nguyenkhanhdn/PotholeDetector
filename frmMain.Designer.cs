45e48f1b7548765e94b282566a357fc24673631d
Khanh Nguyen <khanhdsp@gmail.com>
2023-03-15 20:55:10 +0700

    add new 1

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -32,25 +32,24 @@ namespace PotholeDetector
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.ribbon1 = new System.Windows.Forms.Ribbon();
+            this.ribbonButton1 = new System.Windows.Forms.RibbonButton();
             this.ribbonTab1 = new System.Windows.Forms.RibbonTab();
+            this.ribbonPanel3 = new System.Windows.Forms.RibbonPanel();
             this.ribbonTab2 = new System.Windows.Forms.RibbonTab();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.ribbonPanel1 = new System.Windows.Forms.RibbonPanel();
             this.ribbonPanel2 = new System.Windows.Forms.RibbonPanel();
-            this.ribbonButton1 = new System.Windows.Forms.RibbonButton();
-            this.ribbonButton27 = new System.Windows.Forms.RibbonButton();
-            this.ribbonButton26 = new System.Windows.Forms.RibbonButton();
-            this.ribbonButton25 = new System.Windows.Forms.RibbonButton();
-            this.ribbonPanel3 = new System.Windows.Forms.RibbonPanel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -74,17 +73,31 @@ namespace PotholeDetector
             // 
             this.ribbon1.QuickAccessToolbar.Items.Add(this.ribbonButton1);
             this.ribbon1.RibbonTabFont = new System.Drawing.Font("Trebuchet MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ribbon1.Size = new System.Drawing.Size(1055, 160);
+            this.ribbon1.Size = new System.Drawing.Size(1288, 193);
             this.ribbon1.TabIndex = 0;
             this.ribbon1.Tabs.Add(this.ribbonTab1);
             this.ribbon1.Tabs.Add(this.ribbonTab2);
             this.ribbon1.Text = "ribbon1";
+            // 
+            // ribbonButton1
+            // 
+            this.ribbonButton1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.Image")));
+            this.ribbonButton1.LargeImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.LargeImage")));
+            this.ribbonButton1.MaxSizeMode = System.Windows.Forms.RibbonElementSizeMode.Compact;
+            this.ribbonButton1.Name = "ribbonButton1";
+            this.ribbonButton1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.SmallImage")));
+            this.ribbonButton1.Text = "ribbonButton1";
             // 
             // ribbonTab1
             // 
             this.ribbonTab1.Name = "ribbonTab1";
             this.ribbonTab1.Panels.Add(this.ribbonPanel3);
             this.ribbonTab1.Text = "Pothole Detector";
+            // 
+            // ribbonPanel3
+            // 
+            this.ribbonPanel3.Name = "ribbonPanel3";
+            this.ribbonPanel3.Text = "Pothole Detector";
             // 
             // ribbonTab2
             // 
@@ -93,10 +106,20 @@ namespace PotholeDetector
             this.ribbonTab2.Panels.Add(this.ribbonPanel2);
             this.ribbonTab2.Text = "About";
             // 
+            // ribbonPanel1
+            // 
+            this.ribbonPanel1.Name = "ribbonPanel1";
+            this.ribbonPanel1.Text = "About";
+            // 
+            // ribbonPanel2
+            // 
+            this.ribbonPanel2.Name = "ribbonPanel2";
+            this.ribbonPanel2.Text = "About";
+            // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 160);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 193);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -109,9 +132,18 @@ namespace PotholeDetector
             this.splitContainer1.Panel2.Controls.Add(this.panel1);
             this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.splitContainer1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.splitContainer1.Size = new System.Drawing.Size(1055, 500);
-            this.splitContainer1.SplitterDistance = 351;
+            this.splitContainer1.Size = new System.Drawing.Size(1288, 467);
+            this.splitContainer1.SplitterDistance = 290;
             this.splitContainer1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(290, 467);
+            this.panel2.TabIndex = 1;
             // 
             // panel1
             // 
@@ -119,101 +151,55 @@ namespace PotholeDetector
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(700, 500);
+            this.panel1.Size = new System.Drawing.Size(994, 467);
             this.panel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(351, 500);
-            this.panel2.TabIndex = 1;
-            // 
-            // ribbonPanel1
-            // 
-            this.ribbonPanel1.Name = "ribbonPanel1";
-            this.ribbonPanel1.Text = "About";
-            // 
-            // ribbonPanel2
-            // 
-            this.ribbonPanel2.Name = "ribbonPanel2";
-            this.ribbonPanel2.Text = "About";
-            // 
-            // ribbonButton1
-            // 
-            this.ribbonButton1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.Image")));
-            this.ribbonButton1.LargeImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.LargeImage")));
-            this.ribbonButton1.MaxSizeMode = System.Windows.Forms.RibbonElementSizeMode.Compact;
-            this.ribbonButton1.Name = "ribbonButton1";
-            this.ribbonButton1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.SmallImage")));
-            this.ribbonButton1.Text = "ribbonButton1";
-            // 
-            // ribbonButton27
-            // 
-            this.ribbonButton27.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton27.Image")));
-            this.ribbonButton27.LargeImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton27.LargeImage")));
-            this.ribbonButton27.Name = "ribbonButton27";
-            this.ribbonButton27.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton27.SmallImage")));
-            // 
-            // ribbonButton26
-            // 
-            this.ribbonButton26.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton26.Image")));
-            this.ribbonButton26.LargeImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton26.LargeImage")));
-            this.ribbonButton26.Name = "ribbonButton26";
-            this.ribbonButton26.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton26.SmallImage")));
-            // 
-            // ribbonButton25
-            // 
-            this.ribbonButton25.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton25.Image")));
-            this.ribbonButton25.LargeImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton25.LargeImage")));
-            this.ribbonButton25.Name = "ribbonButton25";
-            this.ribbonButton25.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton25.SmallImage")));
-            this.ribbonButton25.Text = "ABC";
-            // 
-            // ribbonPanel3
-            // 
-            this.ribbonPanel3.Items.Add(this.ribbonButton25);
-            this.ribbonPanel3.Items.Add(this.ribbonButton26);
-            this.ribbonPanel3.Items.Add(this.ribbonButton27);
-            this.ribbonPanel3.Name = "ribbonPanel3";
-            this.ribbonPanel3.Text = "Pothole Detector";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(700, 500);
+            this.pictureBox1.Size = new System.Drawing.Size(994, 467);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.AutoSize = true;
-            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(1003, 9);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(46, 46);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "X";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
             this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(1234, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1055, 660);
+            this.ClientSize = new System.Drawing.Size(1288, 660);
             this.ControlBox = false;
             this.Controls.Add(this.button1);
             this.Controls.Add(this.splitContainer1);
@@ -223,10 +209,15 @@ namespace PotholeDetector
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMain";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -246,11 +237,9 @@ namespace PotholeDetector
         private System.Windows.Forms.RibbonPanel ribbonPanel2;
         private System.Windows.Forms.RibbonButton ribbonButton1;
         private System.Windows.Forms.RibbonPanel ribbonPanel3;
-        private System.Windows.Forms.RibbonButton ribbonButton25;
-        private System.Windows.Forms.RibbonButton ribbonButton26;
-        private System.Windows.Forms.RibbonButton ribbonButton27;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- frmMain.cs
+++ frmMain.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using lobe;
+using lobe.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Media;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,12 +20,188 @@ namespace PotholeDetector
 {
     public partial class frmMain : Form
     {
+        private VideoCaptureDevice captureDevice;
+        private FilterInfoCollection filterInfo;
+        void StartCamera()
+        {
+            try
+            {
+                filterInfo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                captureDevice = new VideoCaptureDevice(filterInfo[0].MonikerString);
+
+                captureDevice.NewFrame += new NewFrameEventHandler(CameraOn);
+                captureDevice.Start();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private void CameraOn(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
         public frmMain()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //string imageDir = Path.Combine(Environment.CurrentDirectory, "..", "..");
+            var bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(bitmap, pictureBox1.ClientRectangle);
+            //ImageFormat imageFormat = imageFormat = ImageFormat.Jpeg;
+            //string filename = imageDir + "\\images\\capture.jpg";
+            //bitmap.Save(filename, imageFormat);
+
+            /*
+            string imageDir = Path.Combine(Environment.CurrentDirectory, "..", "..");
+            string filename = imageDir + "\\Images\\fire1.jpg";
+            */
+            byte[] bytes = ImageToByte2(bitmap);
+            string className = Predict(bytes);
+            ProcessResult(className);
+        }
+        private void ProcessResult(string level)
+        {
+            this.label1.Text = level;
+            //SoundPlayer simpleSound = null;
+            //if (level == "level 1")
+            //{
+            //    textBox1.Text = "Cấp độ cảnh báo: cấp độ 1";
+            //    textBox2.Text = "Bình thường";
+            //    textBox3.Text = "Cảnh báo: TẮT";
+            //    if (simpleSound != null)
+            //    {
+            //        simpleSound.Stop();
+            //    }
+            //    SendToCloud(1, textBox1.Text, textBox2.Text);
+            //}
+            //else if (level == "level 2")
+            //{
+            //    textBox1.Text = "Cấp độ cảnh báo: cấp độ 2";
+            //    textBox2.Text = "Sương mù";
+            //    textBox3.Text = "Cảnh báo: BẬT";
+
+            //    var soundFile = Environment.CurrentDirectory + @"\sound\alarm.wav";
+            //    simpleSound = new SoundPlayer(soundFile);
+            //    simpleSound.Play();
+
+            //    SendToCloud(2, textBox1.Text, textBox2.Text);
+            //}
+            //else if (level == "level 3")
+            //{
+            //    textBox1.Text = "Cấp độ cảnh báo: cấp độ 3";
+            //    textBox2.Text = "Cháy rừng";
+            //    textBox3.Text = "Cảnh báo: BẬT";
+            //    var soundFile = Environment.CurrentDirectory + @"\sound\alarm2.wav";
+            //    simpleSound = new SoundPlayer(soundFile);
+            //    simpleSound.Play();
+            //    SendToCloud(3, textBox1.Text, textBox2.Text);
+            //}
+            //else
+            //{
+            //    textBox1.Text = "Cấp độ cảnh báo: cấp độ 0";
+            //    textBox2.Text = "Bình thường";
+            //    textBox3.Text = "Cảnh báo: TẮT";
+            //    if (simpleSound != null)
+            //    {
+            //        simpleSound.Stop();
+            //    }
+            //    SendToCloud(0, "Cấp độ cảnh báo: cấp độ 0", "Bình thường");
+            //}
+        }
+
+        private void SendToCloud(int level, string desc, string onOff)
+        {
+            try
+            {
+                const string WRITEKEY = "TLCC0QSDXNRF07ZD";
+                string strUpdateBase = "http://api.thingspeak.com/update";
+                string strUpdateURI = strUpdateBase + "?key=" + WRITEKEY;
+                string strField1 = level.ToString();
+                string strField2 = desc;
+                string strField3 = onOff;
+                HttpWebRequest ThingsSpeakReq;
+                HttpWebResponse ThingsSpeakResp;
+
+                strUpdateURI += "&field1=" + strField1;
+                strUpdateURI += "&field2=" + strField2;
+                strUpdateURI += "&field3=" + strField3;
+
+                ThingsSpeakReq = (HttpWebRequest)WebRequest.Create(strUpdateURI);
+
+                ThingsSpeakResp = (HttpWebResponse)ThingsSpeakReq.GetResponse();
+
+                if (!(string.Equals(ThingsSpeakResp.StatusDescription, "OK")))
+                {
+                    Exception exData = new Exception(ThingsSpeakResp.StatusDescription);
+                    //throw exData;
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Text = ex.Message;
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private string Predict(string fileName)
+        {
+            var signatureFilePath = Environment.CurrentDirectory + @"\model\signature.json";
+            var imageToClassify = fileName;
+
+            lobe.ImageClassifier.Register("onnx", () => new OnnxImageClassifier());
+            var classifier = ImageClassifier.CreateFromSignatureFile(new FileInfo(signatureFilePath));
+
+            //var classifier2 = ImageClassifier.CreateFromSignatureFile(new FileInfo(signatureFilePath));
+
+            var results = classifier.Classify(SixLabors.ImageSharp.Image.Load(imageToClassify).CloneAs<Rgb24>());
+            return results.Prediction.Label;
+        }
+
+        private string Predict(byte[] bitmap)
+        {
+            var signatureFilePath = Environment.CurrentDirectory + @"\model\signature.json";
+
+            lobe.ImageClassifier.Register("onnx", () => new OnnxImageClassifier());
+            var classifier = ImageClassifier.CreateFromSignatureFile(new FileInfo(signatureFilePath));
+
+            var results = classifier.Classify(SixLabors.ImageSharp.Image.Load(bitmap).CloneAs<Rgb24>());
+            return results.Prediction.Label;
+        }
+
+        public static byte[] ImageToByte2(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            StartCamera();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            captureDevice.Stop();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
         {
             Application.Exit();
         }
